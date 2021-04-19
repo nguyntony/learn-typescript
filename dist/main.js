9c4ec34c75ab5ffc54cc8c8bfedc8f3b0f376752
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-04-19 10:40:23 -0400

    adding ts notes

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,16 @@
 var a = '1';
 console.log('aaa', a);
+//DECLARING VARIABLES
 // you are able to create variables the same way in js with var, let, const
 var hello = 'world';
 var hi = 'there';
 hi = 'person';
+// hi = 500; you are not able to do thing because 'hi' is a string type
+var greeting = 'salutations';
+greeting = 'bad salutations';
+// greeting = false; this will not work.
+// DECLARING FUNCTIONS
+var getFullName = function (first, last) {
+    return first + " " + last;
+};
+// getFullName(true, 500) you cannot do this because we assigned a type to our params.
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 const a = '1';
 console.log('aaa', a);
 
+//DECLARING VARIABLES
 // you are able to create variables the same way in js with var, let, const
 const hello = 'world';
 
 let hi = 'there';
 hi = 'person';
+// hi = 500; you are not able to do thing because 'hi' is a string type
+
+let greeting: string = 'salutations';
+greeting = 'bad salutations';
+// greeting = false; this will not work.
+
+// DECLARING FUNCTIONS
+const getFullName = (first: string, last: string): string => {
+  return `${first} ${last}`;
+};
+
+// getFullName(true, 500) you cannot do this because we assigned a type to our params.
